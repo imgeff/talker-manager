dc3c9efeb955de1999e42947b9db5519edf296e2
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2022-03-20 22:59:43 -0300

    feat(req-7): Cria o endpoint GET /talker/search

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const getTalkers = require('./middlewares/getTalkers');
 const getTalkerById = require('./middlewares/getTalkerById');
+const searchTalker = require('./middlewares/searchTalker');
 const createTalker = require('./middlewares/createTalker');
 const editTalker = require('./middlewares/editTalker');
 const deleteTalker = require('./middlewares/deleteTalker');
@@ -21,6 +22,8 @@ app.get('/', (_request, response) => {
 app.get('/talker', getTalkers);
 
 app.get('/talker/:id', getTalkerById);
+
+app.get('/talker/search', searchTalker);
 
 app.post('/talker', createTalker);
 
---
+++ middlewares/searchTalker.js
@@ -0,0 +1,5 @@
+const searchTalker = (req, res) => {
+
+};
+
+module.exports = searchTalker;
