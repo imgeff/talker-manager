e3760da20db4ff4dc7d7d12a8b679dfb6be904dc
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2022-03-20 22:02:01 -0300

    feat(req-5): Valida informações de talker no middleware editTalker
    Utiliza o helper talkerValidation para fazer validação das informações do talker, caso não passe em alguma validação retorna uma mensagem de erro referente a validação, se tudo estiver certo retorna undefined

--- middlewares/editTalker.js
+++ middlewares/editTalker.js
@@ -1,5 +1,6 @@
 const { readFile, writeFile } = require('../helpers/fsJSON');
 const tokenValidation = require('../helpers/tokenValidation');
+const talkerValidation = require('../helpers/talkerValidation');
 
 function speakersListEdit(name, age, id, talk) {
   const listSpeakers = readFile('talker.json', true);
@@ -12,15 +13,22 @@ function speakersListEdit(name, age, id, talk) {
 }
 
 const editTalker = (req, res) => {
+  // Constantes
   const { authorization } = req.headers;
   const { id } = req.params;
   const { name, age, talk } = req.body;
+  // Retorno de Validações
   const returnTokenValidation = tokenValidation(authorization);
+  const returnTalkerValidation = talkerValidation(name, age, talk);
   const talkerEdited = speakersListEdit(name, age, id, talk);
-
+  // Condicionais
   if (returnTokenValidation !== undefined) {
     return res.status(401).json({ message: returnTokenValidation });
   }
+  if (returnTalkerValidation !== undefined) {
+    return res.status(400).json({ message: returnTalkerValidation });
+  }
+
   return res.status(200).json(talkerEdited);
 };
 
