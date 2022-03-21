c9577923f322e40499f5438b7ba9af1f0ec2a693
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2022-03-20 22:30:29 -0300

    feat(req-6): Valida token no middleware deleteTalker
    Utiliza helper tokenValidation para validar token

--- middlewares/deleteTalker.js
+++ middlewares/deleteTalker.js
@@ -1,5 +1,12 @@
-const deleteTalker = (req, res) => {
+const tokenValidation = require('../helpers/tokenValidation');
 
+const deleteTalker = (req, res) => {
+  const { authorization } = req.headers;
+  const returnTokenValidation = tokenValidation(authorization);
+
+  if (returnTokenValidation !== undefined) {
+    return res.status(401).json({ message: returnTokenValidation });
+  }
 };
 
 module.exports = deleteTalker;
