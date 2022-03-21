354e4fca9514401c921f8ee7bbfa1244dea3cdd6
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2022-03-21 12:14:35 -0300

    fix(req-5): Edita talker sem alterar id
    Foi utilizado o spread para pegar as informações anteriores do talker e editar com novas, nesse contexto não foi mais preciso inserir id no objeto editado do palestrante, cumprindo o que o requisito pedia

--- helpers/speakersListEdit.js
+++ helpers/speakersListEdit.js
@@ -4,7 +4,7 @@ function speakersListEdit(name, age, id, talk) {
   const listSpeakers = readFile('talker.json', true);
   const oldSpeaker = listSpeakers.find((speaker) => speaker.id === Number(id));
   const indexSpeaker = listSpeakers.indexOf(oldSpeaker);
-  const newSpeaker = { age, id, name, talk };
+  const newSpeaker = { ...oldSpeaker, name, age, talk };
   listSpeakers[indexSpeaker] = newSpeaker;
   writeFile('talker.json', listSpeakers, true);
   return newSpeaker;
